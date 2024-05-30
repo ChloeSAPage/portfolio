483a04d06a8336b82761c2314fcfc21d3b941bdb
ChloeSAPage <chloesapagepersonal@gmail.com>
2024-05-30 17:46:16 +0100

    Add wave to footer

--- src/components/views/Home/Home.jsx
+++ src/components/views/Home/Home.jsx
@@ -3,7 +3,9 @@ import styles from "./Home.module.css";
 function Home() {
     return (
         <div className={styles.wrapper}>
-            <p>Home</p>
+            <h4 className={styles.tagline}>Junior Web Developer</h4>
+            <h1>Hello! My name is Chloe Page</h1>
+            <p>I like making things.</p>
         </div>
     );
 }
